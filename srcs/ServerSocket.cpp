c8126ed4431a623c3087a0e1c392fdf08a4269c8
kamisenin <senelle.camille18@gmail.com>
2026-01-14 15:35:57 +0100

    update ServerSocket constructor to use ServerConfig struct and improve error messages, also added epoll management

--- srcs/ServerSocket.cpp
+++ srcs/ServerSocket.cpp
@@ -1,33 +1,31 @@
-#include "ServerSocket.hpp"
+#include "../includes/ServerSocket.hpp"
 
-ServerSocket::ServerSocket(int sin_port, sa_family_t sin_family)
+ServerSocket::ServerSocket(ServerConfig config, int epollInstance)
 {
 	// sin_family is always AF_INET.
 	// SOCK_STREAM is to tell the socket to use TCP protocol.
-	if ((_socketFd = socket(sin_family, SOCK_STREAM, 0)) == -1) {
+	if ((_socketFd = socket(config.sin_family, SOCK_STREAM, 0)) == -1) {
 		close(_socketFd);
-		throw SocketError("Error during socket creation");
+		throw SocketError("Error during socket creation. Error code : " + itoa(errno));
 	}
 
-	sockaddr_in serverAddress;
-
-	serverAddress.sin_family = sin_family;
-	serverAddress.sin_port = htons(sin_port); /* sin_port is port number. */
-	serverAddress.sin_addr.s_addr = INADDR_ANY; /* INADDR_ANY : accept connection from any IP address */
+	setnonblocking(_socketFd);
+	sockaddr_in serverAddress = setupSocketAddress(config);
 	
-
-	if (bind(_socketFd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+	if (bind(_socketFd, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) == -1) {
 		close(_socketFd);
-		throw BindError("Error while binding adress to the server socket");
+		throw BindError("Error while binding adress to the server socket. Error code : " + itoa(errno));
 	}
 
 	// listen at the server socket and allow 5 connexions at a time
 	if (listen(_socketFd, 5) == -1) {
 		close(_socketFd);
-		throw ListenError("Error while listening");
+		throw ListenError("Error while setting socket to listening. Error code :" + itoa(errno));
 	}
 
-	std::cout << "Server started on port " << sin_port << std::endl;
+	epoll_add(epollInstance, _socketFd, EPOLLIN | EPOLLET);
+
+	std::cout << "Server started on port " << config.sin_port << std::endl;
 }
 
 ServerSocket::~ServerSocket(void)
@@ -38,4 +36,15 @@ ServerSocket::~ServerSocket(void)
 int	ServerSocket::getSocketFd(void)
 {
 	return (_socketFd);
+}
+
+sockaddr_in ServerSocket::setupSocketAddress(ServerConfig config)
+{
+	sockaddr_in serverAddress;
+
+	serverAddress.sin_family = config.sin_family;
+	serverAddress.sin_port = htons(config.sin_port); /* sin_port is port number. */
+	serverAddress.sin_addr.s_addr = INADDR_ANY; /* INADDR_ANY : accept connection from any IP address */
+	
+	return (serverAddress);
 }

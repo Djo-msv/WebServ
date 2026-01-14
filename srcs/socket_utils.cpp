93200d37cd07c941d132fd17aa9eff76b25f6d3b
kamisenin <senelle.camille18@gmail.com>
2026-01-14 15:34:45 +0100

    added utilities for socket management and error handling

---
+++ srcs/socket_utils.cpp
@@ -0,0 +1,41 @@
+#include "../includes/socket_utils.hpp"
+
+int throw_fcntl_error(int fcntl)
+{
+	if (fcntl == -1)
+		throw std::runtime_error("Could not perform fcntl operation error code :" + errno);
+	return (fcntl);
+}
+
+void setnonblocking(int fd)
+{
+	try
+	{
+		int flags = throw_fcntl_error(fcntl(fd, F_GETFL));
+		throw_fcntl_error(fcntl(fd, F_SETFL, flags | O_NONBLOCK));
+	} catch (std::runtime_error e)
+	{
+		close(fd);
+		throw e;
+	}
+}
+
+inline std::string itoa(int nb)
+{
+	std::stringstream ss;
+	ss << nb;
+	return (ss.str());
+}
+
+void	epoll_add(int epollInstance, int fd, u_int32_t flags)
+{
+	epoll_event ev;
+
+	ev.events = flags;
+	ev.data.fd = fd;
+	if (epoll_ctl(epollInstance, EPOLL_CTL_ADD, fd, &ev) == -1)
+	{
+		close(fd);
+		throw std::runtime_error("Could not add '" + itoa(fd) + "' socket to epoll interest list. Error Code :" + itoa(errno));
+	}
+}

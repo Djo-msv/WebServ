595ece8bed069f5e6252a33d42882c5411825717
Kamisenin <131190065+Kamisenin@users.noreply.github.com>
2026-01-17 20:08:56 +0100

    Initialize socket in ServerSocket constructor

--- srcs/Socket/ServerSocket.cpp
+++ srcs/Socket/ServerSocket.cpp
@@ -1,7 +1,7 @@
 #include "ServerSocket.hpp"
 #include <errno.h>
 
-ServerSocket::ServerSocket(ServerConfig config, int epollInstance) : Socket(), _config(config)
+ServerSocket::ServerSocket(ServerConfig config, int epollInstance) : Socket(createSocket()), _config(config)
 {
 	// sin_family is always AF_INET.
 	// SOCK_STREAM is to tell the socket to use TCP protocol.

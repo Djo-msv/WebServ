a746af980d954c2b93b8bb7676856bc4076b5fe5
Johan Massavie <jmassavi@1D2.42angouleme.fr>
2026-01-17 17:01:50 +0100

    Changed the ProcessExecution class for better function encapsulation. Added comment

--- includes/ProcessExecution.hpp
+++ includes/ProcessExecution.hpp
@@ -7,13 +7,14 @@ class ProcessExecution
 	public :
 		ProcessExecution(); /*set all value to 0 */
 		~ProcessExecution(); /* close both pape in case of crash */
-
-		void	startProcess(char **args); /* fork Process and exec CGI, and stock execve output fd */
-
-		int		getProcessFd(int *pid); /* return fd of execve output and set the pid to the actual pid of Process */
+	
+		void startProcess(char **args); /* fork Process and exec CGI, and stock execve output fd */
 		
+		int sendDataProcess(char *bytes); /* send data to the child Process */
+		int readDataProcess(char buffer[BUF_SIZE]); /* Read the output data from the child process, and fill the buffer passed as a parameter. */
+		int getProcessData(int *input, int *output); /* return the pid of the Process and set the fd input and output value */
 	private :
 		int _pipeIn[2]; /* execve input */
-		int	_pipeOut[2]; /* execve output */
-		int	_pid; /* Process pid */
-}
+		int _pipeOut[2]; /* execve output */
+		int _pid; /* Process pid */
+};
--- srcs/Execution/ProcessExecution.cpp
+++ srcs/Execution/ProcessExecution.cpp
@@ -5,9 +5,51 @@ ProcessExecution::ProcessExecution(void) : _pipeIn({-1, -1}), _pipeOut({-1, -1},
 
 ProcessExecution::~ProcessExecution(void)
 {
+	if (_pipeIn[0] != -1)
+		close(_pipeIn[0]);
+	if (_pipeIn[1] != -1)
+		close(_pipeIn[1]);
+	if (_pipeOut[0] != -1)
+		close(_pipeOut[0]);
+	if (_pipeOut[1] != -1)
+		close(_pipeOut[1]);
+}
 
+void ProcessExecution::startProcess(char **args) /* fork Process and exec CGI, and stock execve output fd */
+{
+	if (pipe(_pipeIn) || pipe(_pipeOut)) {
+		// TODO close pipes and throw specific error
+	}
+	if ((_pid = fork()) < 0) {
+		// TODO close pipes and throw specific error
+	}
+	if (_pid == 0) {
+		close(_pipeIn[1]); /* close unused fd */
+		close(_pipeOut[0]);
+		dup2(_pipeIn[0], STDIN_FILENO); /* allows you to change the standard input and output of the child process */
+		dup2(_pipeOut[1], STDOUT_FILENO);
+		close(_pipeIn[0]);
+		close(_pipeOut[1]);
+		execve(args[0], args, NULL); /* This allows a new "args" process to be executed, takes data via the input pipe, and outputs it via the output pipe. */
+		// TODO throw specific error
+	}
 	close(_pipeIn[0]);
-	close(_pipeIn[1]);
-	close(_pipeOut[0]);
 	close(_pipeOut[1]);
+}
+
+int ProcessExecution::sendDataProcess(char *bytes) /* send data to the child Process */
+{
+	size_t	i;
+
+	if (!bytes)
+		return (1);
+	for (i = 0; bytes[i]; i++);
+	write (_pipeIn[1], bytes, i);
+	return (0);
+}
+
+
+int ProcessExecution::readDataProcess(char buffer[BUF_SIZE]) /* Read the output data from the child process, and fill the buffer passed as a parameter. */
+{
+	return (read(_pipeOut[0], buffer, BUF_SIZE));
 }

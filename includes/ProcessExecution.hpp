fb7d56d89aabd5dd8534f5a54fc9049180401941
Johan Massavie <jmassavi@2E6.42angouleme.fr>
2026-01-20 17:41:14 +0100

    add better ancapsulation ProcessExecution

--- includes/ProcessExecution.hpp
+++ includes/ProcessExecution.hpp
@@ -9,15 +9,18 @@ class ProcessExecution
 		ProcessExecution(); /*set all value to 0 */
 		~ProcessExecution(); /* close both pape in case of crash */
 	
-		void startProcess(char **args); /* fork Process and exec CGI, and stock execve output fd */
+		void startProcess(char **args, char **env); /* fork Process and exec CGI, and stock execve output fd */
 		
-		int sendDataProcess(char *bytes); /* send data to the child Process */
-		int readDataProcess(char buffer[BUF_SIZE]); /* Read the output data from the child process, and fill the buffer passed as a parameter. */
+		void readDataProcess(); /* Read the output data from the child process, and fill the buffer passed as a parameter. */
+
+		bool		getStatus();
+		std::string	getResponse();
 
 	private :
-		int _pipeIn[2]; /* execve input */
-		int _pipeOut[2]; /* execve output */
-		int _pid; /* Process pid */
+		int			_pipeOut[2]; /* execve output */
+		int			_pid; /* Process pid */
+		bool 		_status;
+		std::string	_response;
 
 		// Error class
 		class PipeError : public std::runtime_error {

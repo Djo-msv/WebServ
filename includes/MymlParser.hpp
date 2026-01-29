278145b171f56018a2ca24abe103456d6de14b6f
Johan Massavie <jmassavi@2E1.42angouleme.fr>
2026-01-29 17:06:44 +0100

    new parsing, better implementation yml with list and directory is coming

--- includes/MymlParser.hpp
+++ includes/MymlParser.hpp
@@ -1,27 +1,63 @@
 #pragma once
 
 #include "ServerSocket.hpp"
-#include <vector>
-#include <fstream>
-#include <cstdlib>
-#include <sys/stat.h>
-#include <string>
+
+class MymlObject
+{
+	public :
+		MymlObject();
+		~MymlObject();
+
+		int	getAsInt();
+		std::string getAsString();
+
+		std::vector<MymlObject> getAsList();
+		std::map<MymlObject> getAsDictionary();
+		
+		const std::string getKey();
+	private :
+		std::string	value;
+		std::string key;
+		
+		// Error class
+		class WrongType : public std::runtime_error {
+			public :
+				WrongType() : std::runtime_error() {}
+		};
+}
+
+class MymlList : public MymlObject
+{
+	public :
+		std::vector<MymlObject>	getElements();
+
+	private :
+		std::vector<MymlObject>	list;
+}
+
+class MymlDictionary : public MymlObject
+{
+	public
+		MymlObject getMymlObject();
+	
+	private :
+		std::map<std::string, MymlObject>	dictionary;
+}
 
 class MymlParser
 {
 	public :
-		MymlParser(char *files); /* Calling the constructor, verifying the file, and executing the process */
+		MymlParser(char *path); /* Calling the constructor, verifying the file, and executing the process */
 		~MymlParser(); /* Close _confFilefd */
 
-		std::vector<ServerConfig>	getServerConfiguration(); // return vector of serverConfiguration */
-	private :
-		std::ifstream				*_file;
-		std::vector<ServerConfig>	_servConf;
+		std::map<std::string, MymlObject>	getMyml(); // return vector of serverConfiguration */
 
-		void		openFile(const char *path);
-		void		addServerConfiguration(std::string serverName);
-		void		addSetting(std::string line);
-		std::string	readFile();
+	private :
+		std::ifstream		*_file;
+		std::map<std::string, MymlObject>	_myml;
+
+		void	addList();
+		void	addDictionary();
 
 		// Error class
 		class WrongPerm : public std::runtime_error {
--- srcs/Parser/MymlParser.cpp
+++ srcs/Parser/MymlParser.cpp
@@ -1,20 +1,24 @@
 #include "MymlParser.hpp"
 
-MymlParser::MymlParser(char *file)
+MymlParser::MymlParser(char *path)
 {
-	std::string	line;
-	std::string::iterator it;
+	ifstream	*file;
+	std::string	name;
 
-	openFile(file); /* open file, if error, throw an excetpion */
-	while ((line = readFile()) != "")
+	while ((file = openFile(path, name))) /* if directory open each fd of .myml file, else return fd */
 	{
-		for (it = line.begin(); isspace(static_cast<int>(*it)) && it != line.end(); it++) {}; /* found the first char */
-		if (*it == '#') // comments
-			continue ;
-		if (*it == '-' && *(line.end()) == ':')
-			addServerConfiguration(line); /* add serverConfiguration to the vector, and set the name */
-		if (line.find(':', 0))
-			addSetting(line); /* add line to the last confServer vector, if none, throw error */
+		std::string	line;
+
+//		_myml.insert(name, MymlList); // if file already have list
+		while (std::getline(*file, line)) /* read each line inside file */
+		{
+			std::string::iterator it = line.begin();
+			while (it != line.end()) /* tocken */
+			{
+				if (*it == '#')
+					break ;
+			}
+		}
 	}
 }
 
@@ -46,44 +50,4 @@ void	MymlParser::openFile(const char *path)
 	_file = &file;
 }
 
-void	MymlParser::addServerConfiguration(std::string serverName)
-{
-	ServerConfig	server = {0,0,0,0,0};
-
-	std::string::iterator it = serverName.find(':', 0) + serverName.begin();
-	if (it == serverName.begin())
-		return ;
-	serverName.erase(it, serverName.end());
-	for (it = serverName.begin(); (isspace(static_cast<int>(*it)) || *it == '-' ) && it != serverName.end(); it++) {}; /* found the first char */
-	serverName.erase(serverName.begin(), it);
-	server.name = serverName;
-	_servConf.insert(_servConf.end(), server);	
-}
-
-void	MymlParser::addSetting(std::string line)
-{
-	int	i = 0;
-	std::string setting[] = {"sin_port", "sin_family", "cgi_path", "index_file"};
-
-	if (_servConf.empty() == 1)
-		throw BadParsing("at line : " + line + "no Server define before");
-	for (i = 0; i <= 3 && line.find(setting[i], 0) == 0; i++) {};
-	std::string::iterator it = line.find(':', 0) + line.begin() + 1;
-	for (; it != line.end() && isspace(static_cast<int>(*it)); it++) {};
-	line.erase(line.begin(), it);
-	switch (i)
-	{
-		case 0:
-			(*_servConf.end()).sin_port = std::atoi(line.c_str());
-			break ;
-		case 1:	
-			(*_servConf.end()).sin_family = std::atoi(line.c_str());
-			break ;
-		case 2:
-			(*_servConf.end()).cgi_path = line;
-			break ;
-		case 3:
-			(*_servConf.end()).index_file = line;
-			break ;
-	}
 }

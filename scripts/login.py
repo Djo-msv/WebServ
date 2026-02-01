5e755cf678df76e4733d720ede5aa83646ae9d10
styx-trav <naomi.rakotomanga@gmail.com>
2026-02-01 19:34:04 +0100

    modded login.py for new cgi-return requirements, tested, no error-handling

--- scripts/login.py
+++ scripts/login.py
@@ -2,13 +2,15 @@ import warnings
 warnings.filterwarnings('ignore')
 import cgi, cgitb
 
-def success():
-    print ("<html><body><h1>Successfully logged in as %s !</h1></body></html>" % (name))
-
 form = cgi.FieldStorage()
 name = form.getvalue("username")
 password = form.getvalue("password")
 filename = "./logs/logins/" + name
+
+def success():
+    body = "<html><body><h1>Successfully logged in as " + name + " !</h1></body></html>"
+    msg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + str(len(body)) + "\r\n\r\n" + body
+    print ("%s" % msg)
 
 try:
     file = open(filename, "r")
@@ -16,8 +18,10 @@ try:
     if content == password :
         success()
     else:
-        print("<html><body><h1>sorry, this username is already in use and/or the password is incorrect.</h1>")
-        print("<a href=\"/html/login.html\">Retry</a></body></html>")
+        body = "<html><body><h1>sorry, the username " + name + " is already in use and/or the password is incorrect.</h1>"
+        body += "<a href=\"/html/login.html\">Retry</a></body></html>"
+        msg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + str(len(body)) + "\r\n\r\n" + body
+        print ("%s" % msg)
     file.close()
 except:
     file = open(filename, "w")

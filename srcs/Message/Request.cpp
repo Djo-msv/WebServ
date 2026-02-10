86fb286149df099e751b9052bd6c6c4dde8bb81f
styx-trav <naomi.rakotomanga@gmail.com>
2026-02-10 14:27:31 +0100

    forbidden

--- srcs/Message/Request.cpp
+++ srcs/Message/Request.cpp
@@ -64,7 +64,7 @@ std::string	Request::seekFile(std::string &file, ServerConfig &serverConf)
 	 TODO vérifier l'existence et les permissions du fichier, donné, si le fichier donné est un code erreur ou index,
 	 * récupérer le fichier stocké dans serverConf sinon chercher dans le Workdir du serverConf en utilisant le path donné
 	 * le fichier n'existe pas ou n'est pas trouvé renvoyer fichier d'erreur du serverConf, (HttpError, "404 not found") 
-	 * si n'existe pas renvoyer défaut ("403 Forbiden"), si inaccessible renvoyer page HTML hard codé d'erreur de permission ("500 Internal Server Error"),
+	 * si n'existe pas renvoyer défaut ("403 Forbidden"), si inaccessible renvoyer page HTML hard codé d'erreur de permission ("500 Internal Server Error"),
 	 * séparer nom de l'extension, récupérer le cgi depuis serverConf,
 	 * si pas de CGi ou d'extension renvoyer page brut avec exception spécial
 	
@@ -73,7 +73,7 @@ std::string	Request::seekFile(std::string &file, ServerConfig &serverConf)
 
 /*    200 OK
     400 Bad Request
-	403 Forbiden
+	403 Forbidden
     404 Not Found
 	500 Internal Server Error
     501 Not Implemented (method isn't allowed for this server/location, encoding isn't handled, etc.)

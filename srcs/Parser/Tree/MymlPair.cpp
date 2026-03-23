6223bbbc1cc0f3f9260755fd465063997aaa1db1
Johan Massavie <jmassavi@3D7.42angouleme.fr>
2026-03-23 15:20:20 +0100

    fix issu with compilation

--- srcs/Parser/Tree/MymlPair.cpp
+++ srcs/Parser/Tree/MymlPair.cpp
@@ -29,7 +29,7 @@ MymlObject *MymlPair::getValue(void)
 }
 int	MymlPair::getKeyAsInt(void)
 {
-	std::string::iterator it = getKey().begin();
+	std::string::iterator it = _pair.first.begin();
 
 	if (*it == '-')
 		it++;

07fb1c489bd52b2840f223b052f2f7803a9f2cc4
djo <msvdjo@gmail.com>
2026-03-25 20:30:54 +0100

    Update srcs/Parser/Lexer/Parser.cpp

--- srcs/Parser/Lexer/Parser.cpp
+++ srcs/Parser/Lexer/Parser.cpp
@@ -54,7 +54,6 @@ Parser::Parser(const std::string path) : _tree(NULL)
 		_tree = new MymlTree(_tokens);
 
 		_root = ((_tree->getRoot())->getList());
-//			printTree(*it, 0);
 	}
 	catch (const std::runtime_error &e) {
 		std::cout << "\e[1;31m" << "error :" << "\e[0m" << std::endl;
